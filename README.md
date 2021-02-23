2192e71370f2fda301906f85726f4651441729b1
Nikhil Singh <nikhil.singh@nvent.com>
2021-02-23 22:41:23 +0530

    Updated Readme and added server Side

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Graphql2021
+# GraphQL with React and MongoDB
+
+Before you Play
+================
+You need to install the dependencies before you can play.
+
+- You need to navigate to "\Grapql2021" and download the npm packages using the command "npm i".
+- Change the password with DB password in app.js.I bet you dont know so please ask.
+- Make sure you have node and nodemon installed.
+
+Moment of truth 
+================
+If you have done the above steps correctly  
+
+- You need to navigate to "\Grapql2021" and run "nodemon app".
+- For now only running on local, client side yet in progress.
---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require("./schema/schema")
+const mongoose = require('mongoose')
+
+const app = express();
+
+mongoose.connect("mongodb+srv://nikhil_singh:<@password>@gql-nikhil.4zzsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open',()=>{
+    console.log('Connected to database');
+})
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}));
+
+app.listen(4000,()=>{
+    console.log("now listening for request on port 4000")
+});
