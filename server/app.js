bab6c4ad5861247acb485f66991e7d6a607bd272
Nikhil Singh <nikhil.singh@nvent.com>
2021-02-26 14:57:06 +0530

    Made changes for reading from the env files

--- server/app.js
+++ server/app.js
@@ -3,16 +3,16 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require("./schema/schema")
 const mongoose = require('mongoose')
 const cors = require('cors')
-
 const app = express();
-
+require('dotenv').config();
 //allow cross-origin requests
+const mongoDBURL = process.env.mongoose;
+const port = process.env.PORT || 4000
 
 app.use(cors());
-
-mongoose.connect("mongodb+srv://nikhil_singh:<@password>@gql-nikhil.4zzsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open',()=>{
-    console.log('Connected to database');
+    console.log('Connected to MongoDB database');
 })
 
 app.use('/graphql',graphqlHTTP({
@@ -20,6 +20,6 @@ app.use('/graphql',graphqlHTTP({
     graphiql:true
 }));
 
-app.listen(4000,()=>{
-    console.log("now listening for request on port 4000")
+app.listen(port,()=>{
+    console.log("now listening for request on port %s",port)
 });
